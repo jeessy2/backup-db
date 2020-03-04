4f219e5ae162b7d3316f9bde79d2a67931e02bbf
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2020-03-04 22:39:05 +0800

    client & server

---
+++ util/fileUtil.go
@@ -0,0 +1,15 @@
+package util
+
+import "os"
+
+// PathExists Get path exist
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
