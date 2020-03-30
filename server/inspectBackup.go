ad3bc7533d90441f5cdb4c57045ffc1b229a9898
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2020-03-30 11:09:38 +0800

    inspect backup files

---
+++ server/inspectBackup.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"backup-db/client"
+	"backup-db/util"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+)
+
+// InspectBackup Inspect Backup
+func InspectBackup() {
+	for {
+		sleep()
+		inspectInner()
+	}
+}
+
+func inspectInner() {
+	log.Println("Start inspect backup files")
+	// get all projects
+	projects, err := ioutil.ReadDir(client.ParentSavePath)
+	if err != nil {
+		log.Println("Read dir with error :", err)
+	}
+
+	todayString := time.Now().Format("2006-01-02")
+	// delete
+	for _, project := range projects {
+		backupFiles, err := ioutil.ReadDir(client.ParentSavePath + "/" + project.Name())
+		if err != nil {
+			log.Println("Read dir with error :", err)
+			break
+		}
+
+		find := false
+		for _, backupFile := range backupFiles {
+			if strings.Contains(backupFile.Name(), todayString) {
+				find = true
+			}
+		}
+
+		// not find, send email
+		if !find {
+			util.SendEmail("The \""+project.Name()+"\" is not uploaded today", "Please check your project \""+project.Name()+"\"")
+		}
+
+	}
+
+}
+
+func sleep() {
+	sleepHours := 10 - time.Now().Hour()
+	if sleepHours < 0 {
+		sleepHours = 24 + 10 - time.Now().Hour()
+	}
+	log.Println("Run inspect backup files again after", sleepHours, "hours")
+	time.Sleep(time.Hour * time.Duration(sleepHours))
+}
