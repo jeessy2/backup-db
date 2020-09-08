3f1efbcbc7143a5a0ecd15567462b23d31a80adb
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2020-09-08 13:51:28 +0800

    feat: upload over http, not supports custom tcp

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"backup-db/web"
+	"os"
 
 	"log"
 	"net/http"
 	"time"
 )
 
-const port = "9978"
+var defaultPort = "9977"
 
 func main() {
 
@@ -19,11 +20,16 @@ func main() {
 	http.HandleFunc("/", web.WritingConfig)
 	http.HandleFunc("/save", web.Save)
 	http.HandleFunc("/logs", web.Logs)
+	http.HandleFunc("/upload", web.Upload)
 
 	// 运行
 	go web.Run()
 
-	err := http.ListenAndServe(":"+port, nil)
+	if os.Getenv("port") != "" {
+		defaultPort = os.Getenv("port")
+	}
+
+	err := http.ListenAndServe(":"+defaultPort, nil)
 
 	if err != nil {
 		log.Println("启动端口发生异常, 1分钟后自动关闭此窗口", err)
